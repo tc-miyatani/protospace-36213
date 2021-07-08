5aa156ff2f71069f94d81f37fa1636e75608bc3f
tc-miyatani <miyahei38+tc@gmail.com>
2021-07-08 21:57:33 +0900

    ログインしてないければログイン画面に飛ばす処理を削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: :index # ログインしてなければログイン画面へ飛ばす
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
