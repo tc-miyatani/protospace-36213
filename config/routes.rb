a585eaa7470c069f5f4ae2630986ac34094ab0ea
tc-miyatani <miyahei38+tc@gmail.com>
2021-07-09 15:41:08 +0900

    プロトタイプ投稿機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'
-  resources :prototypes, only: [:new, :create]
+  resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy]
   resources :users, only: :show
 end
